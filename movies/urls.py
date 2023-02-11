6dcdfddca1ff26e23b17c21a28551c78447d5365
igorGavr <ragamuffinrogi@gmail.com>
2023-02-11 19:07:53 +0200

    path('movies/', path('movies/<int:pk>/'

--- movies/urls.py
+++ movies/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-
+from . import views
 
 
 urlpatterns = [
-
+    path('movies/', views.MovieListView.as_view()),
+    path('movies/<int:pk>/', views.MovieDetailView.as_view()),
 ]
